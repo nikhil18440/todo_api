c5d674ddc96e3073ee1a4f98a464cd729438730c
Nikhil k <nickjr1032@gmail.com>
2025-10-03 18:21:47 +0530

    changed port

--- src/index.ts
+++ src/index.ts
@@ -136,6 +136,7 @@ mongoose.connect(mongoUrl)
 
 
 // ---------- Start Server ----------
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
